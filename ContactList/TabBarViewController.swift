23864bf50e1d4e675b36d2cdc05b3649a652eb79
hYdrago1 <vanyagoncha@gmail.com>
2025-09-20 17:03:43 +0200

    final

---
+++ ContactList/TabBarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Ivan Honcharov on 20.09.25.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
